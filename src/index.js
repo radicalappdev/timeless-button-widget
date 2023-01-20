8ec27554de22c6331591df762a20c2f0c728ff9c
Joseph Simpson <joe@radicalappdev.com>
2023-01-20 08:55:20 -0500

    First pass at value buttons

--- src/index.js
+++ src/index.js
@@ -1,36 +1,30 @@
 //Here we're importing items we'll need. You can add other imports here.
 
-const durations = [
-  300, // 0:05
-  600, // 0:10
-  1800, // 0:30
-  3600, // 1:00
-  7200, // 2:00
-  10800, // 3:00
+// TODO: Pass this value in from FileMaker
+const data = [
+  { display: "0:05", value: 300 },
+  { display: "0:10", value: 600 },
+  { display: "0:30", value: 1800 },
+  { display: "1:00", value: 3600 },
+  { display: "2:00", value: 7200 },
+  { display: "3:00", value: 10800 },
 ];
 
-//The first function. Remove this.
-const btn = document.querySelector("button");
-btn.onclick = function () {
-  alert("You ran some JavaScript");
+const createButton = (buttonData) => {
+  console.info(buttonData);
+  const button = document.createElement("button");
+  button.type = "button";
+  button.innerHTML = buttonData.display;
+
+  button.onclick = () => {
+    console.log(buttonData.value);
+  };
+
+  return button;
 };
 
-// I found this function here: https://codingbeautydev.com/blog/javascript-convert-seconds-to-hours-and-minutes/
-function toHoursAndMinutes(totalSeconds) {
-  const totalMinutes = Math.floor(totalSeconds / 60);
+const buttonContainer = document.getElementById("button-container");
 
-  const seconds = totalSeconds % 60;
-  const hours = Math.floor(totalMinutes / 60);
-  const minutes = totalMinutes % 60;
-
-  return { h: hours, m: minutes, s: seconds };
-}
-
-// { h: 0, m: 1, s: 0 }
-console.log(toHoursAndMinutes(60));
-
-// { h: 0, m: 16, s: 40 }
-console.log(toHoursAndMinutes(1000));
-
-// { h: 1, m: 10, s: 50 }
-console.log(toHoursAndMinutes(4250));
+data.forEach((element) => {
+  buttonContainer.appendChild(createButton(element));
+});
